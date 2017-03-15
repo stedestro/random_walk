b6d27e25dffc3453c1c2f031e0e4d976f96f75d8
stedestro <stevendestro@users.noreply.github.com>
2017-03-15 12:14:06 +0100

    parametrable 1d random walk

--- random_walk.py
+++ random_walk.py
@@ -1,6 +1,16 @@
 import random
 
 def simple_random_walk_1d(steps, starting_position=0):
+    """
+    Simple random walk in 1 dimension (up/down)
+
+    Arguments:
+        steps (int): number of steps
+        starting_position: where does the walker starts
+
+    Returns:
+        List of 1d coordinates
+    """
     pos = starting_position
     values = [starting_position]
     for x in range(0,steps-1):
@@ -8,6 +18,37 @@ def simple_random_walk_1d(steps, starting_position=0):
             pos += 1
         else:
             pos -= 1
+        values.append(pos)
+    return values
+
+def random_walk_1d(steps, starting_position=0.0, up_ratio=0.5, step_size=1.0):
+    """
+    Parametrable random walk in 1 dimension (up/down)
+
+    Arguments:
+        steps (int): number of steps
+        starting_position (float): where does the walker starts
+        up_ratio (float between 0 and 1.0): percentage of the steps going up
+        step_size (float or tuple): How fat the step goes
+            if step_size is a tuple, randomly select size between
+            [step_size[0], step_size[1]]
+
+    Returns:
+        List of 1d coordinates
+    """
+    pos = starting_position
+    values = [starting_position]
+    for x in range(0,steps-1):
+        if(random.random() < up_ratio):
+            if isinstance(step_size, tuple):
+                pos += random.uniform(step_size[0], step_size[1])
+            else:
+                pos += step_size
+        else:
+            if isinstance(step_size, tuple):
+                pos -= random.uniform(step_size[0], step_size[1])
+            else:
+                pos -= step_size
         values.append(pos)
     return values
 
